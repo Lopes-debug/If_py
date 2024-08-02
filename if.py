cff98bf2e9121a62555612f216b9f75ed016fdbc
Leandro Lopes <lopes.ctrlf5@gmail.com>
2024-08-02 13:45:16 -0300

    Add files via upload

---
+++ if.py
@@ -0,0 +1,31 @@
+# EXERCICIO 1
+
+# vendas_funcionario1 = 1000
+# vendas_funcionario2 = 770
+# vendas_funcionario3 = 2700
+
+# if vendas_funcionario1 >= 1000:
+#     print('O funcionário 1 ganhou {:.0f} de bônus.'.format(vendas_funcionario1*0.1))
+
+# if vendas_funcionario2 >= 1000:
+#     print('O funcionário 2 ganhou {:.0f} de bônus.'.format(vendas_funcionario2*0.1))
+# else:
+#     print('O funcionário 2 ganhou 0 de bônus')
+
+# if vendas_funcionario3 >= 1000:
+#     print('O funcionário 3 ganhou {:.0f} de bônus.'.format(vendas_funcionario3*0.1))
+
+
+# EXERCICIO 2
+
+# vendas_funcionario1 = 1000
+# vendas_funcionario2 = 770
+# vendas_funcionario3 = 2700
+
+# if vendas_funcionario1 >= 2000:
+#     bonus = vendas_funcionario1 * 0.15
+# elif vendas_funcionario1 >= 1000:
+#     bonus = vendas_funcionario1 * 0.10
+# else:
+#     bonus = 0
+# print('O funcionário 1 ganhou {:.0f} de bônus'.format(bonus))
